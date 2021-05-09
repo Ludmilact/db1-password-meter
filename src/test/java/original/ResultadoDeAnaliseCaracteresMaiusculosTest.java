ab5e1f7706ff090f08253543720f8371882e6d95
Ludmilact <ludmilatimoteo@Outlook.com>
2021-05-08 23:26:23 -0300

    Realização de testes unitários analise de caracteres maiúsculos

--- src/test/java/original/ResultadoDeAnaliseCaracteresMaiusculosTest.java
+++ src/test/java/original/ResultadoDeAnaliseCaracteresMaiusculosTest.java
@@ -1,14 +1,13 @@
 package original;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResultadoDeAnaliseCaracteresMaiusculosTest {
 
     @Test
     void testeSemMaiusculas() {
-        var resultado = new ResultadoDeAnaliseCaracteresMaiusculos("asd");
+        var resultado = new ResultadoDeAnaliseCaracteresMaiusculos("teste");
 
         var bonusAtual = resultado.obterBonus();
         var bonusEsperado = 0;
@@ -19,17 +18,21 @@ class ResultadoDeAnaliseCaracteresMaiusculosTest {
         var estadoAtual = resultado.obterEstado();
         var estadoEsperado = TipoEstado.FALHA;
 
+        var operacaoAtual = resultado.obterTipoOperacao();
+        var tipoOperacaoEsperada = TipoOperacao.INCREMENTO;
+
         assertEquals(bonusEsperado, bonusAtual);
         assertEquals(contagemEsperada, contagemAtual);
         assertEquals(estadoEsperado, estadoAtual);
+        assertEquals(tipoOperacaoEsperada, operacaoAtual);
     }
 
     @Test
     void testeUmaMaiuscula() {
-        var resultado = new ResultadoDeAnaliseCaracteresMaiusculos("Asd");
+        var resultado = new ResultadoDeAnaliseCaracteresMaiusculos("Teste");
 
         var bonusAtual = resultado.obterBonus();
-        var bonusEsperado = 4;
+        var bonusEsperado = 8;
 
         var contagemAtual = resultado.obterContagem();
         var contagemEsperada = 1;
@@ -37,8 +40,56 @@ class ResultadoDeAnaliseCaracteresMaiusculosTest {
         var estadoAtual = resultado.obterEstado();
         var estadoEsperado = TipoEstado.SUFICIENTE;
 
+        var operacaoAtual = resultado.obterTipoOperacao();
+        var tipoOperacaoEsperada = TipoOperacao.INCREMENTO;
+
         assertEquals(bonusEsperado, bonusAtual);
         assertEquals(contagemEsperada, contagemAtual);
         assertEquals(estadoEsperado, estadoAtual);
+        assertEquals(tipoOperacaoEsperada, operacaoAtual);
+    }
+
+    @Test
+    void testeVariasLetrasMaiusculas() {
+        var resultado = new ResultadoDeAnaliseCaracteresMaiusculos("TESTe");
+
+        var bonusAtual = resultado.obterBonus();
+        var bonusEsperado = 2;
+
+        var contagemAtual = resultado.obterContagem();
+        var contagemEsperada = 4;
+
+        var estadoAtual = resultado.obterEstado();
+        var estadoEsperado = TipoEstado.EXCEPCIONAL;
+
+        var operacaoAtual = resultado.obterTipoOperacao();
+        var tipoOperacaoEsperada = TipoOperacao.INCREMENTO;
+
+        assertEquals(bonusEsperado, bonusAtual);
+        assertEquals(contagemEsperada, contagemAtual);
+        assertEquals(estadoEsperado, estadoAtual);
+        assertEquals(tipoOperacaoEsperada, operacaoAtual);
+    }
+
+    @Test
+    void testeTodasLetrasMaiusculas() {
+        var resultado = new ResultadoDeAnaliseCaracteresMaiusculos("TESTE");
+
+        var bonusAtual = resultado.obterBonus();
+        var bonusEsperado = 0;
+
+        var contagemAtual = resultado.obterContagem();
+        var contagemEsperada = 5;
+
+        var estadoAtual = resultado.obterEstado();
+        var estadoEsperado = TipoEstado.EXCEPCIONAL;
+
+        var operacaoAtual = resultado.obterTipoOperacao();
+        var tipoOperacaoEsperada = TipoOperacao.INCREMENTO;
+
+        assertEquals(bonusEsperado, bonusAtual);
+        assertEquals(contagemEsperada, contagemAtual);
+        assertEquals(estadoEsperado, estadoAtual);
+        assertEquals(tipoOperacaoEsperada, operacaoAtual);
     }
 }
