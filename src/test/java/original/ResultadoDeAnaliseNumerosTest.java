204de7d466d2f04d5a6e73e3b0ca225f493320b4
Ludmilact <ludmilatimoteo@Outlook.com>
2021-05-09 01:03:03 -0300

    Realização de testes unitários de números

--- src/test/java/original/ResultadoDeAnaliseNumerosTest.java
+++ src/test/java/original/ResultadoDeAnaliseNumerosTest.java
@@ -1,4 +1,93 @@
+package original;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ResultadoDeAnaliseNumerosTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void senhaSemNumeros(){
+
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeros("testeSemNumeros" );
+
+        var contagemEsperada = 0;
+        var bonusEsperado = 0;
+        var operacaoEsperada  = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.FALHA;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+    }
+
+    @org.junit.jupiter.api.Test
+    void senhaComNumeros(){
+    //setup/act
+        var resultado = new ResultadoDeAnaliseNumeros("S3NH4C0MNUM3R0S");
+
+        var contagemEsperada = 5;
+        var bonusEsperado = 20;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.EXCEPCIONAL;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+    }
+
+    @org.junit.jupiter.api.Test
+    void senhaComUmNumero(){
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeros("S3nha");
+
+        var contagemEsperada = 1;
+        var bonusEsperado = 4;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.SUFICIENTE;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+
+    }
+    @org.junit.jupiter.api.Test
+    void senhaComDoisNumeros(){
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeros("S3nh4");
+
+        var contagemEsperada = 2;
+        var bonusEsperado = 8;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.EXCEPCIONAL;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+
+    }
+    @org.junit.jupiter.api.Test
+    void senhaApenasComNumeros(){
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeros("012345");
+
+        var contagemEsperada = 6;
+        var bonusEsperado = 0;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.EXCEPCIONAL;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+
+    }
 }
