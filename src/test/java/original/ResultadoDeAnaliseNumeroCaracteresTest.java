5de431a24b1b57d42c70e072b620178838aebb14
Ludmilact <ludmilatimoteo@Outlook.com>
2021-05-08 22:59:39 -0300

    Realização de testes unitários de numeros de caracteres

--- src/test/java/original/ResultadoDeAnaliseNumeroCaracteresTest.java
+++ src/test/java/original/ResultadoDeAnaliseNumeroCaracteresTest.java
@@ -5,26 +5,88 @@ import static org.junit.jupiter.api.Assertions.*;
 class ResultadoDeAnaliseNumeroCaracteresTest {
 
     @org.junit.jupiter.api.Test
-    void obterBonus() {
+    void senhaSemCaracteres() {
 
-        //setup
+        //setup/act
         var resultado = new ResultadoDeAnaliseNumeroCaracteres("");
 
-        assertEquals(0, resultado.obterContagem());
-        assertEquals(0, resultado.obterBonus());
-        assertEquals(TipoEstado.FALHA, resultado.obterEstado());
-        assertEquals(TipoOperacao.INCREMENTO, resultado.obterTipoOperacao());
+        var contagemEsperada = 0;
+        var bonusEsperado = 0;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.FALHA;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
     }
 
     @org.junit.jupiter.api.Test
-    void obterContagem() {
+    void senhaComUmCaractere() {
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeroCaracteres("A");
+
+        var contagemEsperada = 1;
+        var bonusEsperado = 4;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.FALHA;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
     }
 
     @org.junit.jupiter.api.Test
-    void obterEstado() {
+    void senhaComSeteCaracteres() {
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeroCaracteres("Setedig");
+
+        var contagemEsperada = 7;
+        var bonusEsperado = 28;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.FALHA;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
     }
 
     @org.junit.jupiter.api.Test
-    void obterTipoOperacao() {
+    void senhaComOitoCaracteres() {
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeroCaracteres("Oitodigi");
+
+        var contagemEsperada = 8;
+        var bonusEsperado = 32;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.SUFICIENTE;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
+    }
+
+    @org.junit.jupiter.api.Test
+    void senhaComNoveCaracteres() {
+        //setup/act
+        var resultado = new ResultadoDeAnaliseNumeroCaracteres("Novedigit");
+
+        var contagemEsperada = 9;
+        var bonusEsperado = 36;
+        var operacaoEsperada = TipoOperacao.INCREMENTO;
+        var estadoEsperado = TipoEstado.EXCEPCIONAL;
+
+        //assert
+        assertEquals(contagemEsperada, resultado.obterContagem());
+        assertEquals(bonusEsperado, resultado.obterBonus());
+        assertEquals(estadoEsperado, resultado.obterEstado());
+        assertEquals(operacaoEsperada, resultado.obterTipoOperacao());
     }
 }
