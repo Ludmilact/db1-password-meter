6540f32dc45f2d30306ff202ad5562285adec898
Ludmilact <ludmilatimoteo@Outlook.com>
2021-05-09 17:39:06 -0300

    Criação de um método para contar o número de letras e espaços

--- src/main/java/original/AnalisaRequisitos.java
+++ src/main/java/original/AnalisaRequisitos.java
@@ -16,7 +16,7 @@ public class AnalisaRequisitos {
     public static int obterQuantidadeDeLetrasMinusculas(String senha) {
         int minusculas = 0;
 
-        for (int i = 0; i < senha.length(); i++){
+        for (int i = 0; i < senha.length(); i++) {
             char caractere = senha.charAt(i);
             if (ValidaCaractere.isMinusculo(caractere))
                 minusculas++;
@@ -26,7 +26,7 @@ public class AnalisaRequisitos {
 
     public static int obterQuantidadeDeSimbolos(String senha) {
         int count = 0;
-        for (int i = 0; i < senha.length(); i++){
+        for (int i = 0; i < senha.length(); i++) {
             char caractere = senha.charAt(i);
             if (ValidaCaractere.isSimbolo(caractere))
                 count++;
@@ -36,7 +36,7 @@ public class AnalisaRequisitos {
 
     public static int obterQuantidadeDeNumeros(String senha) {
         int count = 0;
-        for (int i = 0; i < senha.length(); i++){
+        for (int i = 0; i < senha.length(); i++) {
             if (Character.isDigit(senha.charAt(i))) {
                 count++;
             }
@@ -55,12 +55,28 @@ public class AnalisaRequisitos {
         return count;
     }
 
-    public static int obterQuantidadeDeNumerosNoMeio(String senha){
+    public static int obterQuantidadeDeNumerosNoMeio(String senha) {
         int count = 0;
-        for (int i = 1; i < senha.length()-1; i++){
+        for (int i = 1; i < senha.length() - 1; i++) {
             if (Character.isDigit(senha.charAt(i)))
                 count++;
         }
         return count;
+
+    }
+
+    public static int obterQuantidadeDeEspacos(String senha){
+        int count = 0;
+
+       for (int i = 0; i < senha.length(); i++){
+           char caractere = senha.charAt(i);
+           if(ValidaCaractere.isEspaco(caractere))
+                count++;
+        }
+        return count;
+    }
+
+    public static int obterQuantidadeDeLetras(String senha) {
+        return obterQuantidadeDeLetrasMaisculas(senha) + obterQuantidadeDeLetrasMinusculas(senha);
     }
 }
