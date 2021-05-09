1c204f5688c2cd985df13f61867bfefbd938cf00
Ludmilact <ludmilatimoteo@Outlook.com>
2021-05-09 11:05:47 -0300

    Refatoração métodos de validação para estáticos

--- src/main/java/original/ValidaCaractere.java
+++ src/main/java/original/ValidaCaractere.java
@@ -1,24 +1,25 @@
 package original;
 
 public class ValidaCaractere {
-    public boolean isMaisculo(char caractere){
+    public static boolean isMaisculo(char caractere){
         return (caractere >= 'A' && caractere <= 'Z');
     }
 
-    public boolean isMinusculo(char caractere){
-        return (caractere >= 'a' && caractere <= 'z'); }
+    public static boolean isMinusculo(char caractere){
+        return (caractere >= 'a' && caractere <= 'z');
+    }
 
-    public boolean isNumero(char caractere){
+    public static boolean isNumero(char caractere){
         return (caractere >= '0' && caractere <= '9');
     }
 
-    public boolean isEspaco(char caractere){
+    public static boolean isEspaco(char caractere){
         return (caractere == ' ');
     }
 
-    public boolean isSimbolo(char caractere){
-        return !(this.isMaisculo(caractere) || this.isMaisculo(caractere) ||
-                this.isNumero(caractere) || this.isEspaco(caractere));
+    public static boolean isSimbolo(char caractere){
+        return !(isMaisculo(caractere) || isMinusculo(caractere) ||
+                isNumero(caractere) || isEspaco(caractere));
     }
 }
 
