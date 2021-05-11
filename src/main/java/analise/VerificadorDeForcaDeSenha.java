235d3b4a882312a263feb6a0b24c802a22e41383
Ludmilact <ludmilatimoteo@Outlook.com>
2021-05-11 18:55:19 -0300

    Criação de classe para análise força da senha

--- src/main/java/analise/VerificadorDeForcaDeSenha.java
+++ src/main/java/analise/VerificadorDeForcaDeSenha.java
@@ -1,6 +1,6 @@
 package analise;
 
-public class VerificadorDeForcaDeSenha { //classe principal
+public class VerificadorDeForcaDeSenha {
     int forcaDaSenha;
     String senha;
 
@@ -8,9 +8,48 @@ public class VerificadorDeForcaDeSenha { //classe principal
         this.senha = senha;
     }
 
-    void verificar(){
-        var resultadoContagemDeCaracteres = new ResultadoDeAnaliseNumeroCaracteres(senha);
-        forcaDaSenha = resultadoContagemDeCaracteres.obterBonus();
+    public void verificar(){
+
+        var resultadoCaracteresMaiusculos = new ResultadoDeAnaliseCaracteresMaiusculos(senha);
+        var resultadoCaracteresMaiusculosConsecutivos = new ResultadoDeAnaliseCaracteresMaiusculosConsecutivos(senha);
+        var resultadoCaracteresMinusculosConsecutivos = new ResultadoDeAnaliseCaracteresMinusculasConsecutivas(senha);
+        var resultadoCaracteresMinusculos = new ResultadoDeAnaliseCaracteresMinusculos(senha);
+        var resultadoCaracteresRepetidos = new ResultadoDeAnaliseCaracteresRepetidos(senha);
+        var resultadoNumeroDeCaracteres = new ResultadoDeAnaliseNumeroCaracteres(senha);
+        var resultadoNumeros = new ResultadoDeAnaliseNumeros(senha);
+        var resultadoNumerosConsecutivos = new ResultadoDeAnaliseNumerosConsecutivos(senha);
+        var resultadoRequerimentos = new ResultadoDeAnaliseRequerimentos(senha);
+        var resultadoSequenciaDeLetras = new ResultadoDeAnaliseSequenciaDeLetras(senha);
+        var resultadoSequenciaDeNumeros = new ResultadoDeAnaliseSequenciaDeNumeros(senha);
+        var resultadoSequenciaDeSimbolos = new ResultadoDeAnaliseSequenciaDeSimbolos(senha);
+        var resultadoSimbolos = new ResultadoDeAnaliseSimbolos(senha);
+        var resultadoSimbolosENumeros = new ResultadoDeAnaliseSimbolosENumeros(senha);
+        var resultadoSomenteLetras = new ResultadoDeAnaliseSomenteLetras(senha);
+        var resultadoSomenteNumeros = new ResultadoDeAnaliseSomenteNumeros(senha);
+
+
+        var score = new Score();
+
+        score.adicionarResultado(resultadoCaracteresMaiusculos);
+        score.adicionarResultado(resultadoCaracteresMaiusculosConsecutivos);
+        score.adicionarResultado(resultadoCaracteresMinusculosConsecutivos);
+        score.adicionarResultado(resultadoCaracteresMinusculos);
+        score.adicionarResultado(resultadoCaracteresRepetidos);
+        score.adicionarResultado(resultadoNumeroDeCaracteres);
+        score.adicionarResultado(resultadoNumeros);
+        score.adicionarResultado(resultadoNumerosConsecutivos);
+        score.adicionarResultado(resultadoRequerimentos);
+        score.adicionarResultado(resultadoSequenciaDeLetras);
+        score.adicionarResultado(resultadoSequenciaDeNumeros);
+        score.adicionarResultado(resultadoSequenciaDeSimbolos);
+        score.adicionarResultado(resultadoSimbolos);
+
+        var scoreResultado = score.calcularScore();
+        System.out.println(scoreResultado);
+
+        var complexidade = new Complexidade(scoreResultado);
+        var resultadoDeComplexidade = complexidade.calcularComplexidade();
+
     }
 
     String obterComplexidade(){
