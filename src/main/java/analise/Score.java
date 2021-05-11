6137f13b59bb29ca28f2fd2cbc2e7fbc753d4a5a
Ludmilact <ludmilatimoteo@Outlook.com>
2021-05-11 18:57:53 -0300

    Criação de classe para analise de score

---
+++ src/main/java/analise/Score.java
@@ -0,0 +1,27 @@
+package analise;
+
+import analise.ResultadoDeAnalise;
+import tipo.TipoOperacao;
+
+import java.util.ArrayList;
+
+public class Score {
+    ArrayList<ResultadoDeAnalise> resultados = new ArrayList<>();
+
+    public void adicionarResultado(ResultadoDeAnalise resultado) {
+        resultados.add(resultado);
+    }
+
+    public int calcularScore() {
+        int guardarScore = 0;
+        for (int i = 0; i < resultados.size(); i++) {
+            var resultadoAtual = resultados.get(i);
+
+            if (resultadoAtual.obterTipoOperacao() == TipoOperacao.DECREMENTO)
+                guardarScore = guardarScore - resultadoAtual.obterBonus();
+            else
+                guardarScore = guardarScore + resultadoAtual.obterBonus();
+        }
+        return guardarScore;
+    }
+}
